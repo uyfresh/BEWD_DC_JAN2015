6a83fb35a2c90eb2c72631bd8aa792402b3b958a
Nikki Murray <nikki.liz.murray@gmail.com>
2015-02-04 17:42:17 -0500

    submitting homework

---
+++ class03/nikki/disney.rb
@@ -0,0 +1,5 @@
+characters = ["Mickey Mouse", "Aladdin", "Snow White", "Peter Pan", "Simba", "Mulan", "Goofy", "Elsa", "Tarzan", "Donald Duck", "Shere Khan", "Prince Charming", "Cinderella", "Mufasa", "Belle", "Jiminy Cricket"]
+puts characters.sample
+puts characters.index('Elsa')
+puts characters[4]
+puts characters.join('|')
