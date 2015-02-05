6773779715bb815a7116f64ab07674bc615aae25
Kathy Kiely <kathy.kiely@gmail.com>
2015-02-04 19:25:57 -0500

    add class03 files

---
+++ class03/debugging.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+puts "zoheb is awesome!"
+
+binding.pry
+
+favorite _num = 2 + 2
+
+puts "#{favorite_num}"
+
+puts "hello"
+
+favorite_num = 18
+
+puts "goodbye"
---
+++ class04/methods.rb
@@ -0,0 +1,17 @@
+require 'pry'
+def say_hello ()
+	puts "hi"
+
+end
+
+def parrot (name, desire)
+	puts "#{name} wants a #{desire}."
+
+end
+
+def add (a, b)
+	return a + b
+end
+
+
+	binding pry
