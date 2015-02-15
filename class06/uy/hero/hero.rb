5b08abf497c5fd1f85181a772682cca9d5725c54
Uy Tieu <uyfresh@gmail.com>
2015-02-15 00:11:53 -0500

    Update files

---
+++ class06/uy/hero/hero.rb
@@ -0,0 +1,11 @@
+require 'active_record'
+require 'pry'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Base.establish_connection("postgres://localhost/heros")
+
+class Hero < ActiveRecord::Base
+end
+
+binding.pry
