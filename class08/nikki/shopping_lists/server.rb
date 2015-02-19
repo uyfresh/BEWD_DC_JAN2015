4a4c75d87593b095db85d7d4443be74541fbea59
Nikki Murray <nikki.liz.murray@gmail.com>
2015-02-18 21:36:29 -0500

    class 8

---
+++ class08/nikki/shopping_lists/server.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+list = []
+
+# get '/:name' do
+#   @message = "the monkey flies at midnight"
+#   @name = params[:name]
+#   erb :index
+# end
+
+# get '/hello' do
+#   "Hello, thank you for visiting"
+# end
+# get '/cats' do
+#   "cats are the best!"
+# end
+
+# # get '/hello/:name' do
+# #   "Hello #{params[:name]}"
+# # end
+
+# get '/hello/:name/:other_name' do
+#   "Hello #{params[:name]}, you should say hellow to #{params[:other_name]}"
+# end
