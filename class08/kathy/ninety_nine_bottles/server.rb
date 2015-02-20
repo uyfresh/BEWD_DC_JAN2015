deec7eca3ed1295fbd19c0d231bfd753cc3eb98f
Kathy Kiely <kathy.kiely@gmail.com>
2015-02-19 23:46:59 -0500

    updating working files for class 8

---
+++ class08/kathy/ninety_nine_bottles/server.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+get "/verse/:bottles" do
+	@bottles = params[:bottles].to_i 
+	erb :verse
+end
+
+get "/refrain/:bottles" do
+	@bottles = params[:bottles].to_i
+	@next_bottles = @bottles - 1
+
+	erb :refrain
+end
+	
