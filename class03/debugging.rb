73c44e277b7710a20a1041e8012adeb126fbc40b
Tina Stith-Twine <tinatwine@gmail.com>
2015-02-22 14:33:42 -0500

    Clean repo

---
+++ class03/debugging.rb
@@ -0,0 +1,26 @@
+require 'pry'
+<<<<<<< HEAD
+
+puts "zoheb is awesome!"
+
+binding.pry
+
+favorite _num = 2 + 2
+
+puts "#{favorite_num}"
+
+puts "hello"
+
+favorite_num = 18
+
+=======
+puts "sidney is awwesaome!"
+
+fav_num = 2 + 2
+puts " this is cool"
+
+binding.pry
+puts "hello"
+fav_num = 8
+>>>>>>> 9f3dec4f06731da13a92ec30b73d16820e6fa31a
+puts "goodbye"
