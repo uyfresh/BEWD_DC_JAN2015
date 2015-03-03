d90b2520109b9a6fee8226243d75733203e6a929
Kathy Kiely <kathy.kiely@gmail.com>
2015-03-02 20:15:39 -0500

    HW10

---
+++ class10/kathy/dog_db/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dog.create(name:'Fido', image_url: 'http://hdwallpapersfit.com/wp-content/uploads/2015/01/pictures-of-dogs-running.jpg', age: 2)
+Dog.create(name:'Spike', image_url: 'http://hdwallpapersfit.com/wp-content/uploads/2015/01/pictures-of-dogs-sweet-puppy.jpg', age: 1)
+Dog.create(name: 'Tiny', image_url: 'https://www.petfinder.com/wp-content/uploads/2012/11/147083304-dogs-home-alone-all-day-632x475.jpg', age: 4)
