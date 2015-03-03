6981959d8dfec0d3e989fe798ba1e2699f1ed3b0
Uy Tieu <uyfresh@gmail.com>
2015-03-02 21:21:46 -0500

    Update File

--- class10/uy/dog_db/config/routes.rb
+++ class10/uy/dog_db/config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get "/dog" => "dogs#index"
+  get "/dog/new" => "dogs#new"
+  post "/dogs" => "dog#create"
+  get "/dogs/:id" => "dogs#show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
-  # get 'products/:id' => 'catalog#view'
-  get '/about' => 'welcome#about'
+  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
