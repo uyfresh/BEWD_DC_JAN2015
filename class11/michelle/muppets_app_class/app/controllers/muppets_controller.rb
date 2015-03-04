e172f4f9afe8c663a16bd044221064032a557702
Michelle Williams <mnwilliams4@gmail.com>
2015-03-04 17:33:34 -0500

    Complete HW 11

---
+++ class11/michelle/muppets_app_class/app/controllers/muppets_controller.rb
@@ -0,0 +1,48 @@
+class MuppetsController < ApplicationController
+  
+  def index
+    @muppets = Muppet.all
+  end
+
+  def show
+    @muppet = Muppet.find(params[:id])
+  end
+
+  def new
+    @muppet = Muppet.new
+  end
+
+  def create
+    @muppet = Muppet.new(muppet_params)
+
+  if @muppet.save
+    redirect_to @muppet
+  else
+    render :new
+  end
+  end
+
+  def muppet_params
+     params.require(:muppet).permit(:name, :image_url)
+  end
+
+  def edit
+     @muppet = Muppet.find(params[:id])
+  end
+
+  def update
+    @muppet = Muppet.find(params[:id])
+
+  if @muppet.update(muppet_params)
+    redirect_to @muppet
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+    @muppet = Muppet.find(params[:id])
+    @muppet.destroy
+    redirect_to muppets_path
+  end
+end
